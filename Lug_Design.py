0d7caa0d874abde198eddb9bd461a86bca84e245
Neeraj <66212620+neeradnak@users.noreply.github.com>
2023-03-27 11:09:10 +0530

    Update Lug_Design.py

--- Lug_Design.py
+++ Lug_Design.py
@@ -63,7 +63,7 @@ def user_input_features():
                 return " Bending Stress Not Safe, Check again"
 
         def WeldingStress():
-            if WeldingS > Ss:
+            if WeldingS < Ss:
                 return "Welding Stress Safe"
             else:
                 return " Welding Stress Not Safe, Check again"
